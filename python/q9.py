bd409111bb6b141cacd4318f495d6c715cec1a02
AL20105064 <alec.lankford@wipro.com>
2019-12-12 13:23:24 -0600

    Completed some

---
+++ python/q9.py
@@ -0,0 +1,14 @@
+"""
+Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+Example 1:
+Input: 121
+Output: true
+"""
+class Solution:
+    def isPalindrome(self, x:int) -> bool:
+        y = str(x)[::-1]
+        if str(x) == y:
+            return True
+        else:
+            return False
